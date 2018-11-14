1baedaab8d138d1fa8134b399f12b177a33edc1b
Marco Seraphin <seraphin@develope-it.de>
2018-11-14 09:32:52 +0100

    - removed System.Data.SQLClient NuGet package - updated Realm Nuget package

--- AppStandard/AppStandard/App.xaml.cs
+++ AppStandard/AppStandard/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using Acr.UserDialogs;
+﻿using Acr.UserDialogs;
 using AppStandard.Interfaces;
 using AppStandard.Services;
 using FreshMvvm;
@@ -64,7 +59,6 @@ namespace AppStandard
             //masterDetailNav.AddPage<StartPageModel>("Start", null);
             //masterDetailNav.AddPage<SettingsPageModel>("Settings", null);
             //Application.Current.MainPage = masterDetailNav;
-
         }
 
         /// <summary>
@@ -75,44 +69,6 @@ namespace AppStandard
             //FreshIOC.Container.Register<IDatabase, MemoryDatabaseService>().AsSingleton();
             FreshIOC.Container.Register<IDatabase, RealmDatabaseService>().AsSingleton();
             FreshIOC.Container.Register<IUserDialogs>(UserDialogs.Instance);
-
-            //string connectionString = @"Data Source=10.211.55.6;Initial Catalog=DemoDB;Persist Security Info=True;User ID=dbadmin;Password=dbadmin";
-            //string databaseTable = "Person";
-            //string referenceName = "Marco";
-            //string selectQuery = String.Format("SELECT * FROM {0} WHERE Name = '{1}' ", databaseTable, referenceName);
-            //try
-            //{
-            //    using (SqlConnection connection = new SqlConnection(connectionString))
-            //    {
-            //        //open connection
-            //        connection.Open();
-
-            //        SqlCommand command = new SqlCommand(selectQuery, connection)
-            //        {
-            //            Connection = connection,
-            //            CommandText = selectQuery
-            //        };
-
-            //        var dataReader = command.ExecuteReader();
-
-            //        //check if account exists
-            //        var exists = dataReader.HasRows;
-
-            //        if (exists)
-            //        {
-            //            while (dataReader.Read())
-            //            {
-            //                int pkID = dataReader.GetInt32(0);
-            //                string Name = dataReader.GetString(1);
-            //                int Age = dataReader.GetInt32(2);
-            //            }
-            //        }
-            //    }
-            //}
-            //catch (Exception exception)
-            //{
-            //    Console.WriteLine("Error " + exception.Message);
-            //}
         }
 
 		protected override void OnStart ()
