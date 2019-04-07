d87b71c0e7f93e67b81e57e7a6a8b694d86186e1
Marco Seraphin <seraphin@develope-it.de>
2019-04-07 09:10:20 +0200

    - deactivated Android Push Notification

--- AppStandard/AppStandard.Android/MainActivity.cs
+++ AppStandard/AppStandard.Android/MainActivity.cs
@@ -26,71 +26,7 @@ namespace AppStandard.Droid
 
             base.OnCreate(bundle);
 
-//            Set the default notification channel for your app when running Android Oreo
-//            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
-//                {
-//                    Change for your default notification channel id here
-    
-//                    FirebasePushNotificationManager.DefaultNotificationChannelId = "FirebasePushNotificationChannel";
 
-//                    Change for your default notification channel name here
-    
-//                    FirebasePushNotificationManager.DefaultNotificationChannelName = "General";
-//            }
-
-
-//            If debug you should reset the token each time.
-//#if DEBUG
-//            FirebasePushNotificationManager.Initialize(this, true);
-//#else
-//              FirebasePushNotificationManager.Initialize(this,false);
-//#endif
-
-
-//            CrossFirebasePushNotification.Current.OnTokenRefresh += (s, p) =>
-//            {
-//                System.Diagnostics.Debug.WriteLine($"TOKEN : {p.Token}");
-//            };
-
-//            Handle notification when app is closed here
-//            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
-//            {
-//                System.Diagnostics.Debug.WriteLine("Received");
-//            };
-
-//            CrossFirebasePushNotification.Current.OnNotificationOpened += (s, p) =>
-//            {
-//                System.Diagnostics.Debug.WriteLine("Opened");
-//                foreach (var data in p.Data)
-//                {
-//                    System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
-//                }
-
-
-//            };
-
-//            CrossFirebasePushNotification.Current.OnNotificationAction += (s, p) =>
-//            {
-//                System.Diagnostics.Debug.WriteLine("Action");
-
-//                if (!string.IsNullOrEmpty(p.Identifier))
-//                {
-//                    System.Diagnostics.Debug.WriteLine($"ActionId: {p.Identifier}");
-//                    foreach (var data in p.Data)
-//                    {
-//                        System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
-//                    }
-
-//                }
-
-//            };
-
-//            CrossFirebasePushNotification.Current.OnNotificationDeleted += (s, p) =>
-//            {
-
-//                System.Diagnostics.Debug.WriteLine("Deleted");
-
-//            };
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
--- AppStandard/AppStandard.Android/MainApplication.cs
+++ AppStandard/AppStandard.Android/MainApplication.cs
@@ -16,74 +16,72 @@ namespace AppStandard.Droid
     {
         public MainApplication(IntPtr handle, JniHandleOwnership transer) : base(handle, transer)
         {
-        }
+        } 
 
         public override void OnCreate()
         {
             base.OnCreate();
 
-            //Set the default notification channel for your app when running Android Oreo
-            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
-            {
-                //Change for your default notification channel id here
-                FirebasePushNotificationManager.DefaultNotificationChannelId = "FirebasePushNotificationChannel";
+//            //Set the default notification channel for your app when running Android Oreo
+//            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+//            {
+//                //Change for your default notification channel id here
+//                FirebasePushNotificationManager.DefaultNotificationChannelId = "FirebasePushNotificationChannel";
 
-                //Change for your default notification channel name here
-                FirebasePushNotificationManager.DefaultNotificationChannelName = "General";
-            }
-
-
-            //If debug you should reset the token each time.
-#if DEBUG
-            FirebasePushNotificationManager.Initialize(this, true);
-#else
-            FirebasePushNotificationManager.Initialize(this, false);
-#endif
+//                //Change for your default notification channel name here
+//                FirebasePushNotificationManager.DefaultNotificationChannelName = "General";
+//            }
 
 
-            CrossFirebasePushNotification.Current.OnTokenRefresh += (s, p) =>
-            {
-                System.Diagnostics.Debug.WriteLine($"TOKEN : {p.Token}");
-            };
-
-            //Handle notification when app is closed here
-            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
-            {
-                System.Diagnostics.Debug.WriteLine("Received");
-            };
-
-            CrossFirebasePushNotification.Current.OnNotificationOpened += (s, p) =>
-            {
-                System.Diagnostics.Debug.WriteLine("Opened");
-                foreach (var data in p.Data)
-                {
-                    System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
-                }
+//            //If debug you should reset the token each time.
+//#if DEBUG
+//            FirebasePushNotificationManager.Initialize(this, true);
+//#else
+//            FirebasePushNotificationManager.Initialize(this, false);
+//#endif
 
 
-            };
+//            CrossFirebasePushNotification.Current.OnTokenRefresh += (s, p) =>
+//            {
+//                System.Diagnostics.Debug.WriteLine($"TOKEN : {p.Token}");
+//            };
 
-            CrossFirebasePushNotification.Current.OnNotificationAction += (s, p) =>
-            {
-                System.Diagnostics.Debug.WriteLine("Action");
+//            //Handle notification when app is closed here
+//            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
+//            {
+//                System.Diagnostics.Debug.WriteLine("Received");
+//            };
 
-                if (!string.IsNullOrEmpty(p.Identifier))
-                {
-                    System.Diagnostics.Debug.WriteLine($"ActionId: {p.Identifier}");
-                    foreach (var data in p.Data)
-                    {
-                        System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
-                    }
-                }
+//            CrossFirebasePushNotification.Current.OnNotificationOpened += (s, p) =>
+//            {
+//                System.Diagnostics.Debug.WriteLine("Opened");
+//                foreach (var data in p.Data)
+//                {
+//                    System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
+//                }
+//            };
 
-            };
+//            CrossFirebasePushNotification.Current.OnNotificationAction += (s, p) =>
+//            {
+//                System.Diagnostics.Debug.WriteLine("Action");
 
-            CrossFirebasePushNotification.Current.OnNotificationDeleted += (s, p) =>
-            {
+//                if (!string.IsNullOrEmpty(p.Identifier))
+//                {
+//                    System.Diagnostics.Debug.WriteLine($"ActionId: {p.Identifier}");
+//                    foreach (var data in p.Data)
+//                    {
+//                        System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
+//                    }
+//                }
 
-                System.Diagnostics.Debug.WriteLine("Deleted");
+//            };
 
-            };
+//            CrossFirebasePushNotification.Current.OnNotificationDeleted += (s, p) =>
+//            {
+
+//                System.Diagnostics.Debug.WriteLine("Deleted");
+
+//            };
 
 
         }
